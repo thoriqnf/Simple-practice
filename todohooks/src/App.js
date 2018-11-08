641f2655850f1617db468ebb2a78ea4eb9ac54d3
thoriq <thoriqnfaizal@gmail.com>
2018-11-08 20:36:00 +0700

    Finishing todoApp - Completed - Reset

--- todohooks/src/App.js
+++ todohooks/src/App.js
@@ -1,13 +1,35 @@
 import React, { useState } from 'react';
+import Form from './Form'
+
 import './App.css';
 
 export default () => {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+
+  const toggleComplete = i => setTodos(todos.map((todo, k) => k === i ? {
+    ...todo,
+    complete: !todo.complete
+  } : todo))
 
   return (
     <div className="App">
-      <div>count: {count}</div>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <Form 
+        onSubmit={text => setTodos([{text, complete: false}, ...todos ])}
+       />
+      <div>
+        {todos.map(({text, complete}, i) => (
+          <div 
+            key={text} onClick={() => toggleComplete(i)}
+            style={{
+              textDecoration: complete ? 'line-through' : ""
+            }}  
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setTodos([])}>lecet</button>
     </div>
   )
 }
