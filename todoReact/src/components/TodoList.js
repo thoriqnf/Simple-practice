d87b3dde79fc84707f265a6d812ee3d1e13e052d
thoriq <thoriqnfaizal@gmail.com>
2018-11-08 14:47:13 +0700

    2. Display todo

--- todoReact/src/components/TodoList.js
+++ todoReact/src/components/TodoList.js
@@ -16,8 +16,10 @@ export default class TodoList extends React.Component {
     return (
       <div>
         <TodoForm onSubmit={this.addTodo} />
-        {JSON.stringify (this.state.todos)}
+        {this.state.todos.map(todo =>(
+          <div key={todo.id}>{todo.text}</div>
+        ))}
       </div>
-    );
+    ); 
   }
 }  
