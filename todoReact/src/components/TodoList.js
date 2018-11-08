0236554a2d412666e47fd6bb6a3b0ce8cbc21207
thoriq <thoriqnfaizal@gmail.com>
2018-11-08 15:15:46 +0700

    3. Get line through when list complete

--- todoReact/src/components/TodoList.js
+++ todoReact/src/components/TodoList.js
@@ -1,23 +1,40 @@
 import React from "react";
 import TodoForm from './TodoForm';
-
+import Todo from './Todo'; 
 export default class TodoList extends React.Component {
   state = {
     todos: []
   };
 
-  addTodo = todo => {
-    const newTodos = [todo, ...this.state.todos]; 
+  addTodo = todo => { 
     this.setState({
-      todos: newTodos 
-    })
-  }
+      todos: [todo, ...this.state.todos]
+    });
+  };
+  
+  toggleComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id){
+          return{
+            ...todo,
+            complete: !todo.complete
+          };
+        } else {
+          return todo;
+        }
+      })
+    });
+  };
   render(){
     return (
       <div>
         <TodoForm onSubmit={this.addTodo} />
         {this.state.todos.map(todo =>(
-          <div key={todo.id}>{todo.text}</div>
+          <Todo 
+            key={todo.id} 
+            toggleComplete={() => this.toggleComplete(todo.id)}
+            todo={todo}/>
         ))}
       </div>
     ); 
