af6890cf52a48a14a381ad88d03006f5b227bd24
thoriq <thoriqnfaizal@gmail.com>
2018-11-08 14:42:15 +0700

    1. Add todo

---
+++ todoReact/src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import TodoForm from './TodoForm';
+
+export default class TodoList extends React.Component {
+  state = {
+    todos: []
+  };
+
+  addTodo = todo => {
+    const newTodos = [todo, ...this.state.todos]; 
+    this.setState({
+      todos: newTodos 
+    })
+  }
+  render(){
+    return (
+      <div>
+        <TodoForm onSubmit={this.addTodo} />
+        {JSON.stringify (this.state.todos)}
+      </div>
+    );
+  }
+}  
