ded529d35c0d3fb120cf03f506aaa94512d1891b
thoriq <thoriqnfaizal@gmail.com>
2018-11-09 16:45:07 +0700

    finishing setup ESLINT

---
+++ stackcalculator/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'airbnb-base',
+  parser: 'babel-eslint',
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'linebreak-style': 0,
+  },
+  "plugin:react/recommended"
+};
