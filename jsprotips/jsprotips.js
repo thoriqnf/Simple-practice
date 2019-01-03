58c46ecc96fc2c520784cb89b5774d2ca1915ffa
thoriq <thoriqnfaizal@gmail.com>
2019-01-03 12:51:43 +0700

    Adding tips for loop at modern javascript

--- jsprotips/jsprotips.js
+++ jsprotips/jsprotips.js
@@ -123,3 +123,34 @@ pokemon = [...pokemon, "Bulbasaur", "Metapod", "Weedle"];
 
 // Shift
 pokemon = ["Bulbasaur", "Metapod", "Weedle", ...pokemon];
+
+// Loops
+const order = [500, 30, 99, 15, 223];
+
+// Bad Code
+const total = 0;
+const withTax = [];
+const highValue = [];
+for (i = 0; i < onvrdisplaypresentchange.length; i++) {
+  // Reduce
+  total += orders[i];
+
+  // Map
+  withTax.push(orders[i] * 1.1);
+
+  // FIlter
+  if (orders[i] > 100) {
+    highValue.push(orders[i]);
+  }
+}
+ 
+// Good Code
+
+// Reduce
+const total = orders.reduce((acc, cur) => acc = cur)
+
+// Map
+const withTax = orders.map(v => v 1*1)
+
+// Filter
+const highValue = orders.filter( v => v > 100); 
